042b0e01afdd29865329233222971d82773a9bb8
Richard Warren <rwarren92@hotmail.com>
2016-07-11 14:19:22 +0100

    animation

--- public/js/animation.js
+++ public/js/animation.js
@@ -13,3 +13,17 @@ arr.forEach(function(item){
     e.target.className = 'container';
   });
 });
+
+for (let i = 0; i < 13; i++) {
+  let description = 'description' + (i + 1);
+  let image = 'image' + (i + 1);
+  image.addEventListener('mouseover', function(|) {
+    for (let j = 0; j < 13; j++) {
+      document.getElementById(description).className = 'container';
+      document.getElementById(image).className = 'container';
+    }
+    document.getElementById(description).className = 'container containerDark';
+    document.getElementById(image).className = 'container containerDark';
+
+  });
+}
